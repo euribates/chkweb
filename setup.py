4683fd24571fcb7fc7b42e260f9691284d9c80d6
Juan Ignacio Rodriguez de Leon <euribates@gmail.com>
2021-04-17 14:20:14 +0100

    Version 0.1.5

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
+from pathlib import Path
 from setuptools import setup
+
 
 setup(
     name='chkweb',
-    version='0.1.4',
+    version='0.1.5',
+    python_requires='>=3.7',
     py_modules=['chkweb'],
+    author='Juan Ignacio Rodríguez de León',
+    author_email='euribates@gmail.com',
+    license='GNU v.3',
     url="https://github.com/euribates/chkweb.git",
+    description="A very simple web crawler and checker",
+    long_description=Path('README.md').read_text(),
+    long_description_content_type='text/markdown',
     install_requires=[
         'fire',
         'prettyconf',
